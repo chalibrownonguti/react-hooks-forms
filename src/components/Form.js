777b2d7d6834c5004b113ecb32a41d0bd3b030b5
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-08 20:53:58 +0300

    success

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
 function Form() {
+  const [newsletter, setNewsletter] = useState(false);
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
+  }
+
+  function handleNewsletterChange(event) {
+    // .checked, not .value!
+    setNewsletter(event.target.checked);
   }
   
   function handleLastNameChange(event) {
@@ -12,7 +18,15 @@ function Form() {
   }
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input
+        type="checkbox"
+        id="newsletter"
+        onChange={handleNewsletterChange}
+        // {/* checked instead of value */}
+        checked={newsletter}
+      />
+      <input  type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
