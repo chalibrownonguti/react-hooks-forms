65f2d7bc71104058de5065c189b5b09860f103e6
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-08 21:01:15 +0300

    success

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,19 +17,24 @@ function Form() {
     setLastName(event.target.value);
   }
   return (
+    <>
     <form>
-      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
-      <input
-        type="checkbox"
-        id="newsletter"
-        onChange={handleNewsletterChange}
-        // {/* checked instead of value */}
-        checked={newsletter}
-      />
-      <input  type="text" onChange={handleFirstNameChange} value={firstName} />
+    <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+    <input
+      type="checkbox"
+      id="newsletter"
+      onChange={handleNewsletterChange}
+      // {/* checked instead of value */}
+      checked={newsletter}
+    />
+    {/* <button type="submit">Submit</button> */}
+  </form>
+    <form>
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    </>
   );
 }
 
